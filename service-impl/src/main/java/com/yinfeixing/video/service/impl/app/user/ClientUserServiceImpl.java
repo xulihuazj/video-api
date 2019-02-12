4f3bf5a4ada98a5a3fbc6f5d7015fee11332217a
徐礼华 <xulihua@52mamahome.com>
2019-02-12 18:04:06 +0800

    添加 service接口实现类

---
+++ service-impl/src/main/java/com/yinfeixing/video/service/impl/app/user/ClientUserServiceImpl.java
@@ -0,0 +1,13 @@
+package com.yinfeixing.video.service.impl.app.user;
+
+import com.yinfeixing.video.service.app.user.ClientUserService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClientUserServiceImpl implements ClientUserService {
+
+    private Logger logger = LogManager.getLogger(ClientUserServiceImpl.class);
+
+}
