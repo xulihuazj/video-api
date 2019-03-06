8ed9e9f1a28be0bae0656ae36b1c808da557ac53
徐礼华 <xulihua@52mamahome.com>
2019-03-06 14:32:30 +0800

    添加 ElasticJob

---
+++ api/src/main/java/com/yinfeixing/video/job/MySimpleJob.java
@@ -0,0 +1,25 @@
+package com.yinfeixing.video.job;
+
+import com.dangdang.ddframe.job.api.ShardingContext;
+import com.dangdang.ddframe.job.api.simple.SimpleJob;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MySimpleJob implements SimpleJob {
+
+    @Override
+    public void execute(ShardingContext context) {
+        switch (context.getShardingItem()) {
+            case 0:
+                // do something by sharding item 0
+                break;
+            case 1:
+                // do something by sharding item 1
+                break;
+            case 2:
+                // do something by sharding item 2
+                break;
+            // case n: ...
+        }
+    }
+}
